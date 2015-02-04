685399095a5e225212c11402037cdabf9db5f4fb
Jorge Ferreiro <jgferreiro@hotmail.com>
2015-02-04 14:15:06 +0100

    Improving eficiency
    Avoiding to make unnecessary calculations when the Header is not visible

--- js/header.js
+++ js/header.js
@@ -16,19 +16,20 @@
 		titleOpacity = (header.height() - window.scrollY) / window.scrollY;
 		titleMargin =  window.scrollY / header.height();
 
- 		if (window.scrollY > header.height() / 1.2) {
-			opacity += 0.4;
-		}
- 		else if (window.scrollY > header.height() / 1.8) {
-			opacity += 0.2;
-		}
-		else if (window.scrollY > header.height() / 3) {
-			opacity *= 1.3;
-		} 
-		if(window.scrollY > 0)
-		{
-	 		headerContent.css('opacity', titleOpacity); 
-			headerContent.css('margin-bottom', - titleMargin * 90); 
-	 		headerBlur.css('opacity', opacity); 
-		}
+		if (window.scrollY < header.height()) {
+			
+	 		if (window.scrollY > header.height() / 1.2) 
+				opacity += 0.4;
+	 		else if (window.scrollY > header.height() / 1.8)
+				opacity += 0.2; 
+			else if (window.scrollY > header.height() / 3) 
+				opacity *= 1.3;
+
+			if (window.scrollY > 0) {
+		 		headerContent.css('opacity', titleOpacity); 
+				headerContent.css('margin-bottom', - titleMargin * 90); 
+		 		headerBlur.css('opacity', opacity); 
+			}
+		}  
+ 		
 	});
