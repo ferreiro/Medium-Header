95e57477951e0c84806855671358c673beb765f0
Jorge Ferreiro <me@jgferreiro.com>
2015-05-15 14:02:14 +0200

    Israel's idea: include variables inside

--- js/header.js
+++ js/header.js
@@ -1,22 +1,22 @@
 
   	/****************************************
   	*** Jorge Ferreiro made this Scripts  ***
-  	*** Github: github.com/jgferreiro *******
+  	*** Github: github.com/ferreiro *********
   	*** Website: jgferreiro.com   ***********
   	*** Copyright Jorge Ferreiro  ***********
   	*****************************************/
  	
 	var header = $('.Header'), 
 		headerBlur = $('.HeaderBlurImage'), 
-		headerContent = $('.HeaderContent');
+		headerTitle = $('.HeaderContent');
 
 	$(document).scroll(function(e)
 	{ 
-		opacity =  window.scrollY / header.height();
-		titleOpacity = (header.height() - window.scrollY) / window.scrollY;
-		titleMargin =  window.scrollY / header.height();
-
 		if (window.scrollY < header.height()) {
+
+			opacity =  window.scrollY / header.height();
+			titleOpacity = (header.height() - window.scrollY) / window.scrollY;
+			titleMargin =  window.scrollY / header.height();
 			
 	 		if (window.scrollY > header.height() / 1.2) 
 				opacity += 0.4;
@@ -26,8 +26,8 @@
 				opacity *= 1.3;
 
 			if (window.scrollY > 0) {
-		 		headerContent.css('opacity', titleOpacity); 
-				headerContent.css('margin-bottom', - titleMargin * 90); 
+		 		headerTitle.css('opacity', titleOpacity); 
+				headerTitle.css('margin-bottom', - titleMargin * 90); 
 		 		headerBlur.css('opacity', opacity); 
 			}
 		}  
